5479c182bc1acc96fa90f03cddc3eb66a579d7b5
Ken Pham <kenpham@Kens-MacBook-Pro.local>
2025-12-01 09:52:48 -0500

    Trigger fresh Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/static';
 import sitemap from '@astrojs/sitemap';
 
-// Cache buster: 2025-11-29 21:15
+// Cache buster: 2025-12-01 09:53
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.kenpham.ca',
