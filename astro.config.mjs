4d4cf5b3e797d50a3c2c4ed6424819776f114445
Ken Pham <kenpham@Kens-MacBook-Pro.local>
2025-12-01 10:14:30 -0500

    Switch to serverless adapter to fix routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 
-// Cache buster: 2025-12-01 09:53
+// Cache buster: 2025-12-01 09:58
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.kenpham.ca',
+  output: 'server',
   adapter: vercel(),
   integrations: [sitemap()]
 });
